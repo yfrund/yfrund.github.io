d7d9ac30429f9e9cf11cf656979670de9cca260c
yuliia.frund <yuliia.frund@uzh.ch>
2024-09-14 20:01:40 +0200

    add script

---
+++ assets/scripts/bingo.js
@@ -0,0 +1,41 @@
+
+
+function parseText(text) {
+  const pattern = /^(\d+)-(\d+)$/;
+
+  if (pattern.test(text)) {
+    const match = text.match(pattern);
+    const start = parseInt(match[1]);
+    const end = parseInt(match[2]);
+
+    const nums = Array.from({length: end - start + 1}, (v,i) => start + i);
+
+    return nums;
+  } else {
+
+    const strings = text.split('@@');
+    return strings;
+  }
+}
+
+function randomGenerate(inp) {
+  let parsed = parseText(inp);
+  const original = [...parsed]
+
+  console.log("Press Enter to print a random item. Press Esc to exit.");
+
+  process.stdin.on("keypress", (str, key) => {
+    if (key.name === "escape") {
+      console.log("\nExiting");
+      process.exit();
+    } else if (key.name === "return") {
+      if (!parsed.length) {
+        parsed = [...original];
+        console.log("All items have been used - starting over.");
+      }
+      const randomIndex = Math.floor(Math.random() * parsed.length);
+      const randomItem = parsed.splice(randomIndex, 1)[0];
+      console.log(randomItem);
+    }
+  });
+}
